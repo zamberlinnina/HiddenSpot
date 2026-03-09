2a4ebd4ed3cd8af0c5e44db081a4f255c9cef9b8
Dunja Međurečan <dunja.medurecan@fer.unizg.hr>
2026-03-09 16:54:55 +0100

    Repo set-up

---
+++ backend/src/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+
+const app = express();
+app.use(express.json());
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`API running on http://localhost:${port}`);
+});
